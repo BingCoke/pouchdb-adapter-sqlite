3c99209c6a2ed5e9d9a1dabff5b243b65699b2d7
bingcoke <bingcoke@192.168.0.104>
2025-04-06 18:44:50 +0800

    style:remove space

--- packages/pouchdb-adapter-opsqlite/src/processBinary.ts
+++ packages/pouchdb-adapter-opsqlite/src/processBinary.ts
@@ -1,7 +1,6 @@
 import { getBlobForArrayBuffer } from 'react-native-blob-jsi-helper';
 export const createBlob = (binary: string, type: string) => {
   const buffer = Buffer.from(binary, 'binary');
-
   // @ts-ignore
   const blob = getBlobForArrayBuffer(buffer.buffer);
   return blob;
